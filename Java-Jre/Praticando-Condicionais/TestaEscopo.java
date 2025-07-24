704752ffe61f2b19b9bd8b1ebcf5e78c5591436c
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-07-24 01:20:34 -0300

    Create TestaEscopo.java

---
+++ Java-Jre/Praticando-Condicionais/TestaEscopo.java
@@ -0,0 +1,25 @@
+
+public class TestaEscopo {
+
+	public static void main(String[] args) {
+		System.out.println("Testando Condicionais");
+
+		int idade = 20;
+		int quantidadePessoas = 3;
+		boolean acompanhado;
+
+		if (quantidadePessoas >= 2) {
+			acompanhado = true;
+		} else {
+			acompanhado = false;
+		}
+
+		if (idade >= 18 || acompanhado) {
+
+			System.out.println("Seja bem-vindo");
+		} else {
+			System.out.println("Infelizmente você não pode entrar");
+		}
+
+	}
+}
