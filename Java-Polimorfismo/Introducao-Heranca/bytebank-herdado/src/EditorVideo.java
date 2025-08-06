40d2745a75271642cfb457f86ebb3a5df102de6c
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-06 13:29:47 -0300

    Delete Java-Polimorfismo/Introducao-Heranca/bytebank-herdado/src/EditorVideo.java

--- Java-Polimorfismo/Introducao-Heranca/bytebank-herdado/src/EditorVideo.java
+++
@@ -1,9 +0,0 @@
-
-public class EditorVideo extends Funcionario {
-	
-	public double getBonificacao() {
-		System.out.println("Chamando o metodo de bonificaçao do Editor de Video");
-		return 150;
-	}
-	
-}
