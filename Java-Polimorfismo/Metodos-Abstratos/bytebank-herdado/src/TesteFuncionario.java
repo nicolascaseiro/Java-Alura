f39f0694311c7b30538b176dbd0dce4674418b81
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 01:12:11 -0300

    Update TesteFuncionario.java

--- Java-Polimorfismo/Metodos-Abstratos/bytebank-herdado/src/TesteFuncionario.java
+++ Java-Polimorfismo/Metodos-Abstratos/bytebank-herdado/src/TesteFuncionario.java
@@ -3,7 +3,7 @@ public class TesteFuncionario {
 	
 	public static void main(String[] args) {
 		
-		Funcionario nick = new Gerente();
+		Gerente nick = new Gerente();
 		nick.setNome("Nicolas Caseiro");
 		nick.setCpf("99988877766");
 		nick.setSalario(10000.00);
