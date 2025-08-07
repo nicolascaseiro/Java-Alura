37388982d25d790212fa36ac5535c6dd89cf85c2
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 00:46:48 -0300

    Update ContaPoupanca.java

--- Java-Polimorfismo/Uso-Construtores/bytebank-herdado/src/ContaPoupanca.java
+++ Java-Polimorfismo/Uso-Construtores/bytebank-herdado/src/ContaPoupanca.java
@@ -1,7 +1,7 @@
 public class ContaPoupanca extends Conta {
 
-    public ContaPoupanca(int agencia, int numero, Cliente titular) {
-        super(agencia, numero, titular);
+    public ContaPoupanca(int agencia, int numero) {
+        super(agencia, numero;
     }
     
 }
