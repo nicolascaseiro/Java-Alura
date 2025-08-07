727da21f217bdb7d3eb32f2251934b51a35bcff6
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 20:46:36 -0300

    Create EditorVideo.java

---
+++ Java-Polimorfismo/Praticando/EditorVideo.java
@@ -0,0 +1,9 @@
+
+public class EditorVideo extends Funcionario {
+	
+	public double getBonificacao() {
+		System.out.println("Chamando o metodo de bonifica�ao do Editor de Video");
+		return 150;
+	}
+	
+}
