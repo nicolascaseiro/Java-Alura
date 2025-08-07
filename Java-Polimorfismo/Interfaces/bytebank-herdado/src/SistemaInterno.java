fec3aa928924ebd7c286b562f513c4cfcb491e77
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 17:48:22 -0300

    Update SistemaInterno.java

--- Java-Polimorfismo/Interfaces/bytebank-herdado/src/SistemaInterno.java
+++ Java-Polimorfismo/Interfaces/bytebank-herdado/src/SistemaInterno.java
@@ -3,13 +3,13 @@ public class SistemaInterno {
 	
 	private int senha = 2222;
 	
-	public void autentica(Autenticavel fa) {
-		boolean autenticou = fa.autentica(this.senha);
+	public void autentica(Gerente g) {
+		boolean autenticou = g.autentica(this.senha);
 		
 		if (autenticou){
 			System.out.println("Pode entrar no sistema");		
 		} else {
-			System.out.println("Não pode entrar no sistema");
+			System.out.println("NÃ£o pode entrar no sistema");
 		}	
 	}
 }
