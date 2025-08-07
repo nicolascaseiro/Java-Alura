6bebae4b6ab794fd94f53b0ad988c19090e1f07d
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 18:10:53 -0300

    Update Gerente.java

--- Java-Polimorfismo/Interfaces/bytebank-herdado/src/Gerente.java
+++ Java-Polimorfismo/Interfaces/bytebank-herdado/src/Gerente.java
@@ -1,5 +1,5 @@
 
-public class Gerente extends FuncionarioAutenticavel {
+public class Gerente extends Funcionario implements Autenticavel {
 	
 	public double getBonificacao() {
 		System.out.println("Chamando o método de bonificacao do GERENTE")
