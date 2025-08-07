27006ae0cd9dc366321f1f23282f724d8b9eecc0
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 17:42:26 -0300

    Create ControleBonificacao.java

---
+++ Java-Polimorfismo/Metodos-Abstratos/ControleBonificacao.java
@@ -0,0 +1,15 @@
+
+public class ControleBonificacao {
+
+	double soma;
+	
+	public double getSoma() {
+		return this.soma;
+	}
+	
+	public void registra(Funcionario f) {
+			this.soma += f.getBonificacao();
+	}
+	
+	
+}
