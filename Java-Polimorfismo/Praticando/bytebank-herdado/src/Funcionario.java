c32bc556034214c4720df9acf199c51654a15a37
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 20:47:09 -0300

    Update Funcionario.java

--- Java-Polimorfismo/Praticando/bytebank-herdado/src/Funcionario.java
+++ Java-Polimorfismo/Praticando/bytebank-herdado/src/Funcionario.java
@@ -4,8 +4,7 @@ public abstract class Funcionario {
 	private String nome;
 	private String cpf;
 	private double salario;
-	
-	// metodo sem corpo, não há implementação
+
 	public abstract double getBonificacao();
 	
 	public String getNome() {
