5794d3f93626d6018a052ddb63bab00a14992d47
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 17:52:28 -0300

    Update Administrador.java

--- Java-Polimorfismo/Interfaces/bytebank-herdado/src/Administrador.java
+++ Java-Polimorfismo/Interfaces/bytebank-herdado/src/Administrador.java
@@ -1,28 +1,22 @@
+public class Administrador extends Funcionario {
 
-public class Administrador extends Funcionario implements Autenticavel{
+	private int senha;
 
-	
-	private AutenticacaoUtil autenticador;
-	
-	public Administrador() {
-		this.autenticador = new AutenticacaoUtil();
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
-		
 	@Override
 	public double getBonificacao() {
-		return 0;
+		return 50;
 	}
-	
-	@Override
-	public void setSenha(int senha) {
-		this.autenticador.setSenha(senha);;		
-	}
-
-	@Override
-	public boolean autentica(int senha) {
-		return this.autenticador.autentica(senha);
-	}
-	
 
 }
