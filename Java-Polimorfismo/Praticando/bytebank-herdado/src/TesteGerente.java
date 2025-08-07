7a81eba4d1498a345c572858a080b3b3cf5a6681
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 20:49:32 -0300

    Update TesteGerente.java

--- Java-Polimorfismo/Praticando/bytebank-herdado/src/TesteGerente.java
+++ Java-Polimorfismo/Praticando/bytebank-herdado/src/TesteGerente.java
@@ -3,23 +3,23 @@ public class TesteGerente {
 	
 	public static void main(String[] args) {
 		
+		Autenticavel referencia = new Cliente();
+		
 		Gerente g1 = new Gerente();
-		
-		// aqui esta herdando os atributos da classe mae funcionario por causa do extends
-		g1.setNome("Lucianne");
-		g1.setCpf("123.456.789-00");
-		g1.setSalario(5000);		
+		g1.setNome("Marco");
+		g1.setCpf("235568413");
+		g1.setSalario(5000.0);		
 				
-		System.out.println("Nome da Gerente: " + g1.getNome());
-		System.out.println("Cpf da Gerente: " + g1.getCpf());
-		System.out.println("Salario da Gerente: " + g1.getSalario());
+		System.out.println(g1.getNome());
+		System.out.println(g1.getCpf());
+		System.out.println(g1.getSalario());
 
-		//aqui esta criando uma variavel e comparando "=" com a outra		
-		g1.setSenha(12345);		
-		boolean autenticou = g1.autentica(12345);
+		g1.setSenha(2222);
+		boolean aunteticou = g1.autentica(2222);
+
 		System.out.println(autenticou);
-		
-		System.out.println(g1.getBonificacao());
+
+		System.out.println(g1.getBonificacao());_
 		
 	}
 
