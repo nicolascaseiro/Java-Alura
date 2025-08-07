3658cbd4347b79dbeb202ab8c73eb78e37637898
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 01:01:49 -0300

    Update TesteReferencias.java

--- Java-Polimorfismo/Metodos-Abstratos/bytebank-herdado/src/TesteReferencias.java
+++ Java-Polimorfismo/Metodos-Abstratos/bytebank-herdado/src/TesteReferencias.java
@@ -6,15 +6,19 @@ public class TesteReferencias {
 		Gerente g1 = new Gerente();
 		g1.setNome("Marcos");
 		g1.setSalario(5000);
+
+		Funcionario f = new Funcionario();
+		f.setSalario(2000); 
 		
 		EditorVideo ev = new EditorVideo();
 		ev.setSalario(2500);
-		
+
 		Designer d = new Designer();
-		d.setSalario(2000); 
+		f.setSalario(2000); 
 		
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(g1);
+		controle.registra(f);
 		controle.registra(ev);
 		controle.registra(d); 
 		
