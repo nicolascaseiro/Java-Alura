ab9c210bba74644496126364dcaed92be6eaf2fa
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 01:03:59 -0300

    Update EditorVideo.java

--- Java-Polimorfismo/Metodos-Abstratos/bytebank-herdado/src/EditorVideo.java
+++ Java-Polimorfismo/Metodos-Abstratos/bytebank-herdado/src/EditorVideo.java
@@ -2,7 +2,7 @@
 public class EditorVideo extends Funcionario {
 	
 	public double getBonificacao() {
-		System.out.println("Chamando o metodo de bonificaçao do Editor de Video");
+		System.out.println("Chamando o metodo de bonificacao do Editor de Video");
 		return 150;
 	}
 	
