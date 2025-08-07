99f448ac3fd34123aa8abb270a43eda8c32c6b98
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 18:00:15 -0300

    Update TesteSistema.java

--- Java-Polimorfismo/Interfaces/bytebank-herdado/src/TesteSistema.java
+++ Java-Polimorfismo/Interfaces/bytebank-herdado/src/TesteSistema.java
@@ -6,15 +6,11 @@ public class TesteSistema {
 		Gerente g = new Gerente();
 		g.setSenha(2222);
 		
-		Cliente c = new Cliente();
-		c.setSenha(1234);
-		
-		Administrador a = new Administrador();
+		Administrador adm = new Administrador();
 		a.setSenha(2222);
 		
 		SistemaInterno si = new SistemaInterno();
 		si.autentica(g);
-		si.autentica(c);
-		si.autentica(a);
+		si.autentica(adm);
 	}
 }
