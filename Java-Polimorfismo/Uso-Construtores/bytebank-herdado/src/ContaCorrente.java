797a6c130e06df3c313aa5cac03f9516b608a47b
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 00:49:59 -0300

    Create  ContaCorrente.java

---
+++ Java-Polimorfismo/Uso-Construtores/bytebank-herdado/src/ContaCorrente.java
@@ -0,0 +1,13 @@
+
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente(int agencia, int numero) {
+        super(agencia, numero, titular);
+    }
+
+    @Override
+    public boolean saca(double valor) {
+        double valorASacar = valor + 0.2;
+        return super.saca(valor);
+    }
+}
