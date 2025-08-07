a43a7fe6c101fe3620f87dca6186c4c7c4502b07
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-08-07 17:57:32 -0300

    Update Gerente.java

--- Java-Polimorfismo/Interfaces/bytebank-herdado/src/Gerente.java
+++ Java-Polimorfismo/Interfaces/bytebank-herdado/src/Gerente.java
@@ -1,21 +1,8 @@
 
-public class Gerente extends Funcionario {
-	
-	private int senha;
-
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-	
-	public boolean autentica(int senha) {
-		if (this.senha == senha){
-			return true;
-		} else {
-			return false;
-		}
-	}
+public class Gerente extends FuncionarioAutenticavel {
 	
 	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificacao do GERENTE")
 		return super.getSalario();
 	}
 
